a15b9d5a40282c619ae4397f243046e514fc7b6b
unknown <istiak@ermg.co>
2023-02-12 22:15:52 +0600

    cors header added

--- ecommerce/server.py
+++ ecommerce/server.py
@@ -1,6 +1,6 @@
 {
     "apps": [{
-        "name": "djnago_with_pm2",
+        "name": "server",
         "script": "manage.py",
         "args": ["runserver", "0.0.0.0:8000"],
         "exec_mode": "fork",
